12c9c93cb11ef941d3259f86cd19fac94947fc57
AndreMesa003 <amesalles01@outlook.com>
2024-05-01 16:27:53 +0200

    Game

---
+++ main.py
@@ -0,0 +1,12 @@
+import math
+import random
+from player import *
+def main():
+    player = Player()
+    print(f"Your name is {player.name} !")
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ player.py
@@ -0,0 +1,5 @@
+
+
+class Player:
+    def __init__(self):
+        self.name = input("What will be your character's name?: ")
